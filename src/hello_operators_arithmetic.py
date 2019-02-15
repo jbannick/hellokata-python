66463072bc5217beabee589229cb64b8b5a08b89
John Bannick <jbannick@7128.com>
2019-02-15 14:06:02 -0500

    Create hello_operators_arithmetic.py

---
+++ src/hello_operators_arithmetic.py
@@ -0,0 +1,35 @@
+"""Kata for Arithmetic Operators."""
+print("Hello Arithmetic Operators!")
+
+a = 10
+b = 20
+
+x = a + b
+print(x)
+
+x = a - b
+print(x)
+
+x = a * b
+print(x)
+
+x = b / a
+print(x)
+
+x = b % a
+print(x)
+
+x = a ** b
+print(x)
+
+x = 9//2
+print(x)
+
+x = 9.0//2.0
+print(x)
+
+x = -11//-4
+print(x)
+
+x = -11.0//3
+print(x)
