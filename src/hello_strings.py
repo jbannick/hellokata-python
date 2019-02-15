2842fbbd172bbe050f16ba488644f245a981c048
John Bannick <jbannick@7128.com>
2019-02-15 11:44:13 -0500

    Create hello_strings.py

---
+++ src/hello_strings.py
@@ -0,0 +1,9 @@
+"""Kata for basic string functions"""
+mystring = 'Hello Strings!'
+
+print(mystring)          # Prints complete string
+print(mystring[0])       # Prints first character of the string
+print(mystring[2:5])     # Prints characters starting from 3rd to 5th
+print(mystring[2:])      # Prints string starting from 3rd character
+print(mystring * 2)      # Prints string two times
+print(mystring + "TEST") # Prints concatenated string
