4230d7cf313a91d775bdbf833f20f835234fe031
John Bannick <jbannick@7128.com>
2019-02-15 15:07:46 -0500

    Create hello_conditionals.py

---
+++ src/hello_conditionals.py
@@ -0,0 +1,22 @@
+"""Kata for conditionals"""
+print("Hello Conditionals!")
+
+if 1 < 2:
+    print("It's True")
+
+if 2 < 1:
+    print("It's True, but wrong!")
+else:
+    print("It's False")
+
+
+if 'a' == 'b':
+    if 1 > 2:
+        print("Result 1")
+    else:
+        print("Result 2")
+else:
+    if 1 < 2:
+        print("Result 3")
+    else:
+        print("Result 4")
