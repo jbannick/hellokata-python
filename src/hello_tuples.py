ab6dfc3aff7e0cdee2c842167f361cc1c4d120e3
John Bannick <jbannick@7128.com>
2019-02-15 11:59:02 -0500

    Create hello_tuples.py

---
+++ src/hello_tuples.py
@@ -0,0 +1,12 @@
+"""Kata for tuples"""
+print("Hello Tuples!")
+
+mytuple = ('abcd', 786, 2.23, 'john', 70.2)
+tinytuple = (123, 'john')
+
+print(mytuple)             # Prints complete tuple
+print(mytuple[0])          # Prints first element of the tuple
+print(mytuple[1:3])        # Prints elements starting from 2nd till 3rd
+print(mytuple[2:])         # Prints elements starting from 3rd element
+print(tinytuple * 2)       # Prints tuple two times
+print(mytuple + tinytuple) # Prints concatenated tuple
