c0ed3184a4f16fa1e54185b3dde84b0967de056e
John Bannick <jbannick@7128.com>
2019-02-15 14:06:35 -0500

    Create hello_operators_comparison.py

---
+++ src/hello_operators_comparison.py
@@ -0,0 +1,12 @@
+"""Kata for comparison operators"""
+print("Hello Comparison Operators!")
+
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
