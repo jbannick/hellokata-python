de3b3bd591364771454477a2064b3cb4651c7429
John Bannick <jbannick@7128.com>
2019-02-15 14:11:15 -0500

    Create hello_operators_membership.py

---
+++ src/hello_operators_membership.py
@@ -0,0 +1,12 @@
+"""Kata for membership operators"""
+print("Hello Membership Operators!")
+
+a = 5
+b = 10
+mylist = [1, 2, 3, 4]
+
+print(a in mylist)
+
+print(b not in mylist)
+
+print(3 in mylist)
