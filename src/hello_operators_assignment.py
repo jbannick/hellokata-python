21b552fb9e514d5878688f0e1625df48f49f8a63
John Bannick <jbannick@7128.com>
2019-02-15 14:07:08 -0500

    Create hello_operators_assignment.py

---
+++ src/hello_operators_assignment.py
@@ -0,0 +1,36 @@
+"""Kata for assignment operators"""
+print("Hello Assignment Operators!")
+
+a = 10
+b = 20
+
+x = a + b
+print(x)
+
+x = a
+x += b
+print(x)
+
+x = b
+x -= a
+print(x)
+
+x = a
+x *= b
+print(x)
+
+x = b
+x /= a
+print(x)
+
+x = b
+x %= a
+print(x)
+
+x = b
+x **= a
+print(x)
+
+x = b
+x //= a
+print(x)
